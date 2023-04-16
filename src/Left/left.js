45312e0af7cea251cfa64375307845b4f95ae14b
Christopher L Ruckman <cruckman900@cardinal.wju.edu>
2023-04-16 02:03:13 -0400

    styling left menu

--- src/Left/left.js
+++ src/Left/left.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Nav, Overlay, OverlayTrigger, PopoverBody, PopoverHeader } from "react-bootstrap";
 import { Link, useLocation } from 'react-router-dom';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { 
@@ -6,8 +7,8 @@ import {
 } from '@fortawesome/free-solid-svg-icons';
 import classes from './left.module.css';
 
-
-export default function left() {
+const Left = () => {
+    const location = useLocation();
 
     const myIcons = {
         house: faHouse,
@@ -18,20 +19,22 @@ export default function left() {
     
     return (
         <React.Fragment>
-            <div className={classes.left}>
-                <Link to={"/"} eventkey="/" href="#" className={classes.menuButtonBox}>
+            <Nav className={classes.left} defaultActiveKey="#" activeKey={location.pathname} justify>
+                <Nav.Link as={Link} to={"/"} eventKey="/" href="#" className={classes.menuButtonBox}>
                     <FontAwesomeIcon className={classes.menuButton} icon={myIcons['house']}></FontAwesomeIcon>
-                </Link>
-                <Link to={"/userSettings"} href="/UserSettings" className={classes.menuButtonBox}>
+                </Nav.Link>
+                <Nav.Link as={Link} to={"/userSettings"} eventKey="/userSettings" href="/UserSettings" className={classes.menuButtonBox}>
                     <FontAwesomeIcon className={classes.menuButton} icon={myIcons['userGear']}></FontAwesomeIcon>
-                </Link>
-                <Link to={"/friendSettings"} href="/FriendSettings" className={classes.menuButtonBox}>
+                </Nav.Link>
+                <Nav.Link as={Link} to={"/friendSettings"} eventKey="/friendSettings" href="/FriendSettings" className={classes.menuButtonBox}>
                     <FontAwesomeIcon className={classes.menuButton} icon={myIcons['usersGear']}></FontAwesomeIcon>
-                </Link>
-                <Link to={"/groupSettings"} href="/GroupSettings" className={classes.menuButtonBox}>
+                </Nav.Link>
+                <Nav.Link as={Link} to={"/groupSettings"} eventKey="/groupSettings" href="/GroupSettings" className={classes.menuButtonBox}>
                     <FontAwesomeIcon className={classes.menuButton} icon={myIcons['peopleRoof']}></FontAwesomeIcon>
-                </Link>
-            </div>
+                </Nav.Link>
+            </Nav>
         </React.Fragment>
     );
 }
+
+export default Left;
