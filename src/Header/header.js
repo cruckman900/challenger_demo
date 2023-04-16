27a3cb9d5f499b5173724352176b43fa29fe159d
Christopher L Ruckman <cruckman900@cardinal.wju.edu>
2023-04-16 03:02:35 -0400

    font color changes

--- src/Header/header.js
+++ src/Header/header.js
@@ -6,7 +6,7 @@ export default function header() {
         <React.Fragment>
             <div className={classes.header}>
                 <span className={classes.headerText}>
-                    <a href="https://cruckman.com" alt="cruckman.com" target="_blank" rel="noreferrer">Powered by CRUCKMAN.COM</a>
+                    <a href="https://cruckman.com" alt="cruckman.com" target="_blank" rel="noreferrer">By <i>CRUCKMAN</i></a>
                 </span>
             </div>
         </React.Fragment>
--- src/Left/left.js
+++ src/Left/left.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Nav, Overlay, OverlayTrigger, PopoverBody, PopoverHeader } from "react-bootstrap";
+import { Nav } from "react-bootstrap";
 import { Link, useLocation } from 'react-router-dom';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { 
@@ -20,16 +20,16 @@ const Left = () => {
     return (
         <React.Fragment>
             <Nav className={classes.left} defaultActiveKey="#" activeKey={location.pathname} justify>
-                <Nav.Link as={Link} to={"/"} eventKey="/" href="#" className={classes.menuButtonBox}>
+                <Nav.Link as={Link} to={"/"} eventKey="/" href="#">
                     <FontAwesomeIcon className={classes.menuButton} icon={myIcons['house']}></FontAwesomeIcon>
                 </Nav.Link>
-                <Nav.Link as={Link} to={"/userSettings"} eventKey="/userSettings" href="/UserSettings" className={classes.menuButtonBox}>
+                <Nav.Link as={Link} to={"/userSettings"} eventKey="/userSettings" href="/UserSettings">
                     <FontAwesomeIcon className={classes.menuButton} icon={myIcons['userGear']}></FontAwesomeIcon>
                 </Nav.Link>
-                <Nav.Link as={Link} to={"/friendSettings"} eventKey="/friendSettings" href="/FriendSettings" className={classes.menuButtonBox}>
+                <Nav.Link as={Link} to={"/friendSettings"} eventKey="/friendSettings" href="/FriendSettings">
                     <FontAwesomeIcon className={classes.menuButton} icon={myIcons['usersGear']}></FontAwesomeIcon>
                 </Nav.Link>
-                <Nav.Link as={Link} to={"/groupSettings"} eventKey="/groupSettings" href="/GroupSettings" className={classes.menuButtonBox}>
+                <Nav.Link as={Link} to={"/groupSettings"} eventKey="/groupSettings" href="/GroupSettings">
                     <FontAwesomeIcon className={classes.menuButton} icon={myIcons['peopleRoof']}></FontAwesomeIcon>
                 </Nav.Link>
             </Nav>
