8eff447432e21fe787cc196074d8ab483ee43bc6
Christopher L Ruckman <cruckman900@cardinal.wju.edu>
2023-04-23 16:00:23 -0400

    edited manifest to add name/short name and removed space for Title

--- src/Top/top.js
+++ src/Top/top.js
@@ -7,7 +7,7 @@ export default function top() {
     return (
         <React.Fragment>
             <div className={classes.top}>
-                <span className={`${classes.topText} ${font.Kochire}`}>Chatterbox SM</span>
+                <span className={`${classes.topText} ${font.Kochire}`}>ChatterboxSM</span>
                 <span className={classes.logo}><img src={logo} /></span>
             </div>
         </React.Fragment>
