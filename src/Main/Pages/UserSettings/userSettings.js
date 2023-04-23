e21e78e3f2f408a555919276aa7561ccf80a7751
Christopher L Ruckman <cruckman900@cardinal.wju.edu>
2023-04-23 17:04:56 -0400

    button edit and named inputs

--- src/Main/Pages/UserSettings/userSettings.js
+++ src/Main/Pages/UserSettings/userSettings.js
@@ -3,6 +3,7 @@ import DefaultPage from '../../../UI/DefaultPage/DefaultPage';
 import BodyHeader from "../../../UI/BodyHeader/BodyHeader";
 import LeftLabelInput from "../../../UI/LeftLabelInput/LeftLabelInput";
 import Label from "../../../UI/Label/Label";
+import Button from "../../../UI/Button/Button";
 import classes from './userSettings.module.css';
 import { Container } from "react-bootstrap";
 
@@ -12,32 +13,99 @@ export default function userSettings(props) {
             <Container className={classes.body}>
                 <BodyHeader>Account Information</BodyHeader>
                 <div className={classes.formRow}>
-                    <LeftLabelInput labelClassName={classes.labelText} inputClassName={classes.inputStyle} labelText="First Name" inputType="text">{props.firstName}</LeftLabelInput>
-                    <LeftLabelInput labelClassName={classes.labelText} inputClassName={classes.inputStyle} labelText="Last Name" inputType="text">{props.lastName}</LeftLabelInput>
+                    <LeftLabelInput name="txtFirstName" inputType="text"
+                        labelClassName={classes.labelText}
+                        inputClassName={classes.inputStyle}
+                        labelText="First Name"
+                    >
+                        {props.firstName}
+                    </LeftLabelInput>
+                    <LeftLabelInput name="txtLastName" inputType="text"
+                        labelClassName={classes.labelText}
+                        inputClassName={classes.inputStyle}
+                        labelText="Last Name"
+                    >
+                        {props.lastName}
+                    </LeftLabelInput>
                 </div>
                 <div className={classes.formRow}>
                     <Label className={classes.label} text="Age Range" />
-                    <LeftLabelInput labelClassName={classes.labelTextShort} labelText="Under 18" inputType="radio" name="age" value="under18" checked="checked"></LeftLabelInput>
-                    <LeftLabelInput labelClassName={classes.labelTextShort} labelText="18 or Older" inputType="radio" name="age" value="18orOlder"></LeftLabelInput>
+                    <LeftLabelInput name="age" inputType="radio"
+                        labelClassName={classes.labelTextShort}
+                        labelText="Under 18"
+                        value="under18"
+                        checked="checked"
+                    />
+                    <LeftLabelInput name="age" inputType="radio"
+                        labelClassName={classes.labelTextShort}
+                        labelText="18 or Older"
+                        value="18orOlder"
+                    />
                 </div>
                 <div className={classes.formRow}>
-                    <LeftLabelInput labelClassName={classes.labelText} inputClassName={classes.inputStyle} labelText="Email (optional)" inputType="email">{props.email}</LeftLabelInput>
+                    <LeftLabelInput name="txtEmail" inputType="email"
+                        labelClassName={classes.labelText}
+                        inputClassName={classes.inputStyle}
+                        labelText="Email (optional)"
+                    >
+                        {props.email}
+                    </LeftLabelInput>
                 </div>
                 <div className={classes.formRow}>
-                    <LeftLabelInput labelClassName={classes.labelText} inputClassName={classes.inputStyle} labelText="Username" inputType="text">{props.username}</LeftLabelInput>
+                    <LeftLabelInput name="txtUsername" inputType="text"
+                        labelClassName={classes.labelText}
+                        inputClassName={classes.inputStyle}
+                        labelText="Username"
+                    >
+                        {props.username}
+                    </LeftLabelInput>
                 </div>
                 <div className={classes.formRow}>
-                    <LeftLabelInput labelClassName={classes.labelText} inputClassName={classes.inputStyle} labelText="Password" inputType="password">{props.password}</LeftLabelInput>
-                    <LeftLabelInput labelClassName={classes.labelText} inputClassName={classes.inputStyle} labelText="Confirm Password" inputType="password"></LeftLabelInput>
+                    <LeftLabelInput name="txtPassword" inputType="password"
+                        labelClassName={classes.labelText}
+                        inputClassName={classes.inputStyle}
+                        labelText="Password"
+                    >
+                        {props.password}
+                    </LeftLabelInput>
+                    <LeftLabelInput name="txtConfirmPassword" inputType="password"
+                        labelClassName={classes.labelText}
+                        inputClassName={classes.inputStyle}
+                        labelText="Confirm Password"
+                    />
                 </div>
 
                 <BodyHeader>Location Information (optional)</BodyHeader>
                 <div className={classes.formRow}>
-                    <LeftLabelInput labelClassName={classes.labelText} inputClassName={classes.inputStyle} labelText="City" inputType="text">{props.city}</LeftLabelInput>
-                    <LeftLabelInput labelClassName={classes.labelText} inputClassName={classes.inputStyle} labelText="State/Province" inputType="text">{props.state}</LeftLabelInput>
+                    <LeftLabelInput name="txtCity" inputType="text"
+                        labelClassName={classes.labelText}
+                        inputClassName={classes.inputStyle}
+                        labelText="City"
+                    >
+                        {props.city}
+                    </LeftLabelInput>
+                    <LeftLabelInput name="txtState" inputType="text"
+                        labelClassName={classes.labelText}
+                        inputClassName={classes.inputStyle}
+                        labelText="State/Province"
+                    >
+                        {props.state}
+                    </LeftLabelInput>
                 </div>
                 <div className={classes.formRow}>
-                    <LeftLabelInput labelClassName={classes.labelText} inputClassName={classes.inputStyle} labelText="Country" inputType="text">{props.country}</LeftLabelInput>
+                    <LeftLabelInput name="txtCountry" inputType="text"
+                        labelClassName={classes.labelText}
+                        inputClassName={classes.inputStyle}
+                        labelText="Country"
+                    >
+                        {props.country}
+                    </LeftLabelInput>
+                </div>
+
+                <BodyHeader>&nbsp;</BodyHeader>
+                <div className={classes.formRow}>
+                    <Button type="submit" name="btnSubmit" value="Submit" />
+                    <Button type="button" name="btnClear" value="Clear" />
                 </div>
             </Container>
         </DefaultPage>
