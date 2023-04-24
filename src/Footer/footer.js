2c5c004000f9a3282c1905c7a4580dfa84bcf8f3
Christopher L Ruckman <cruckman900@cardinal.wju.edu>
2023-04-24 18:43:33 -0400

    changes to header and footer font

--- src/Footer/footer.js
+++ src/Footer/footer.js
@@ -4,7 +4,7 @@ import classes from './footer.module.css';
 export default function footer() {
     return (
         <footer className={classes.footer}>
-            <i>&copy;{new Date().getFullYear()} CHATTERBOXSM - CRUCKMAN.COM &mdash; ALL RIGHTS RESERVED</i>
+            &copy;{new Date().getFullYear()} CHATTERBOXSM - CRUCKMAN.COM &mdash; ALL RIGHTS RESERVED
         </footer>
     );
 }
