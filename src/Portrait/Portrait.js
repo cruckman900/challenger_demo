6dc0b6cfb5767d1ace90fe094d0dfea27b3ba9c9
Christopher L Ruckman <cruckman900@cardinal.wju.edu>
2023-04-25 03:15:45 -0400

    'try rotating'

--- src/Portrait/Portrait.js
+++ src/Portrait/Portrait.js
@@ -7,7 +7,7 @@ export default function Portrait() {
     return (
         <Fragment>
             <div className={classes.headerText}>This site is designed for wider screens</div>
-            <div className={classes.bodyText}>Please rotate your device 90 degrees so that this site will be displayed correctly.</div>
+            <div className={classes.bodyText}>Please try rotating your device 90 degrees so that this site will be displayed correctly.</div>
             <div className={classes.goRotate}><FontAwesomeIcon className={classes.iconRotate} icon={faRotate} /></div>
         </Fragment>
     );
