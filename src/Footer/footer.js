f3c989a546dc3c22c3d02584f34d3583dfdb9591
Christopher L Ruckman <cruckman900@cardinal.wju.edu>
2023-04-25 03:09:16 -0400

    removed need for media queries.  prettied up the Portrait page

--- src/Footer/footer.js
+++ src/Footer/footer.js
@@ -4,7 +4,7 @@ import classes from './footer.module.css';
 export default function footer() {
     return (
         <footer className={classes.footer}>
-            &copy;{new Date().getFullYear()} CHATTERBOXSM - CRUCKMAN.COM &mdash; ALL RIGHTS RESERVED
+            &copy;{new Date().getFullYear()} CHATTERBOXSM &mdash; ALL RIGHTS RESERVED
         </footer>
     );
 }
--- src/Main/Pages/Home/home.js
+++ src/Main/Pages/Home/home.js
@@ -7,7 +7,7 @@ import classes from './home.module.css';
 export default function home() {
     return (
         <Fragment>
-            <DefaultPage headerText="Welcome to ChatterboxSM!">
+            <DefaultPage headerText="Welcome Home!">
                 <div>
                     This place is for communities to be created and joined by all that wish to be welcomed in.  You can join communities, create a friend's group,
                     or talk individually with friends that you make, all in one spot!  In the future, if I can fund it, I would like to incorporate audio capabilities
