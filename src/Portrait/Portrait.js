46dc08d58c4810f257855acbb2193b3992228b4e
Christopher L Ruckman <cruckman900@cardinal.wju.edu>
2023-04-25 03:19:26 -0400

    added a period to the header

--- src/Portrait/Portrait.js
+++ src/Portrait/Portrait.js
@@ -6,7 +6,7 @@ import { faRotate } from '@fortawesome/free-solid-svg-icons';
 export default function Portrait() {
     return (
         <Fragment>
-            <div className={classes.headerText}>This site is designed for wider screens</div>
+            <div className={classes.headerText}>This site is designed for wider screens.</div>
             <div className={classes.bodyText}>Please try rotating your device 90 degrees so that this site will be displayed correctly.</div>
             <div className={classes.goRotate}><FontAwesomeIcon className={classes.iconRotate} icon={faRotate} /></div>
         </Fragment>
