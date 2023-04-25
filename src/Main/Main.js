6cee94f0a2ddb43fd930c9dbf0bdd089a52e6633
Christopher L Ruckman <cruckman900@cardinal.wju.edu>
2023-04-24 21:24:55 -0400

    added top navigation and page stubs

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,9 +1,13 @@
 import React, { Component } from "react";
 import { Routes, Route } from 'react-router-dom';
 import Home from './Pages/Home/home';
+import Login from './Pages/Login/login';
+import Donations from './Pages/Donations/donations';
+import Suggestions from './Pages/Suggestions/suggestions';
 import UserSettings from './Pages/UserSettings/userSettings';
 import FriendSettings from './Pages/FriendSettings/friendSettings';
 import GroupSettings from './Pages/GroupSettings/groupSettings';
+import CommunitySettings from './Pages/CommunitySettings/communitySettings';
 import classes from './main.module.css';
 
 class Main extends Component {
@@ -12,9 +16,13 @@ class Main extends Component {
             <div className={`${classes.main} ${classes.scrollable} ${classes.noScrollbars}`}>
                 <Routes>
                     <Route exact path="/" element={ <Home /> } />
+                    <Route path="/login" element={ <Login /> } />
+                    <Route path="/donations" element={ <Donations /> } />
+                    <Route path="/suggestions" element={ <Suggestions /> } />
                     <Route path="/userSettings" element={ <UserSettings /> } />
                     <Route path="/friendSettings" element={ <FriendSettings /> } />
                     <Route path="/groupSettings" element={ <GroupSettings /> } />
+                    <Route path="/communitySettings" element={ <CommunitySettings /> } />
                 </Routes>
             </div>
         );
--- src/Top/top.js
+++ src/Top/top.js
@@ -2,13 +2,24 @@ import React from "react";
 import classes from './top.module.css';
 import font from '../fonts.module.css';
 import logo from '../assets/images/logo.png';
+import { Nav, Navbar } from 'react-bootstrap';
+import { useLocation, Link } from 'react-router-dom';
 
-export default function top() {
+export default function Top() {
+    const location = useLocation();
+
     return (
         <React.Fragment>
             <div className={classes.top}>
                 <span className={`${classes.topText} ${font.Kochire}`}>ChatterboxSM</span>
                 <span className={classes.logo}><img src={logo} /></span>
+                <Navbar key="top" className={classes.navbar}>
+                    <Nav activeKey={location.pathname}>
+                        <Nav.Link as={Link} to={"/login"} eventKey="/login" href="#login" className={classes.topNav}>Login</Nav.Link>
+                        <Nav.Link as={Link} to={"/donate"} eventKey="/donate" href="#donate" className={classes.topNav}>Donate</Nav.Link>
+                        <Nav.Link as={Link} to={"/suggestions"} eventKey="/suggestions" href="#suggestions" className={classes.topNav}>Suggestions</Nav.Link>
+                    </Nav>
+                </Navbar>
             </div>
         </React.Fragment>
     );
