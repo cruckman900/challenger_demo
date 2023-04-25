8d521b68dc488d67a2c0035eff69d571e7729c81
Christopher L Ruckman <cruckman900@cardinal.wju.edu>
2023-04-25 02:14:27 -0400

    updated the Portrait page - looks spiffy

--- src/Portrait/Portrait.js
+++ src/Portrait/Portrait.js
@@ -1,11 +1,14 @@
 import React, { Fragment } from "react";
-import classes from './Portrait.classes.css'
+import classes from './Portrait.module.css'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faRotate } from '@fortawesome/free-solid-svg-icons';
 
 export default function Portrait() {
     return (
         <Fragment>
-            <div className={classes.header}>This site is designed for wide screens</div>
-            <div className={classes.body}>Please rotate your device 90 degrees so that this site will be displayed correctly.</div>
+            <div className={classes.headerText}>This site is designed for wide screens</div>
+            <div className={classes.bodyText}>Please rotate your device 90 degrees so that this site will be displayed correctly.</div>
+            <div className={classes.goRotate}><FontAwesomeIcon className={classes.iconRotate} icon={faRotate} /></div>
         </Fragment>
     );
 }
