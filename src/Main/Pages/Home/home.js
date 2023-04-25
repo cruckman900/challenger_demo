19fac5b2b81ec196d2e58e7bc4f1c2d5f98f261c
Christopher L Ruckman <cruckman900@cardinal.wju.edu>
2023-04-25 05:37:02 -0400

    editing text in note

--- src/Main/Pages/Home/home.js
+++ src/Main/Pages/Home/home.js
@@ -11,9 +11,10 @@ export default function home() {
                     or talk individually with friends that you make, all in one spot!  In the future, if I can fund it, I would like to incorporate audio capabilities
                     so that your encounters are a little more personal than reading text on a screen.
                 </div>
-                <Note headerText="NOTE: Be aware that many parts of this site move.">
+                <Note headerText="NOTE: Many parts of this site move.">
                     If you are on a phone, for instance, the icon bar to the left scrolls up and down, as well as items that 
-                    will appear in this area and in the right column.  Perhaps take some time to navigate around and get familiar with the interface.
+                    will appear in this area and in the right column.  There are collapsable/expandable items such as those in the right column.
+                    Perhaps take some time to navigate around and get familiar with the interface if you need to.
                 </Note>
             </DefaultPage>
         </Fragment>
