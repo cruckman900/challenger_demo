2653e9dc3e7acead98f4e67cda4dd4d9cd7f6c1e
Christopher L Ruckman <cruckman900@cardinal.wju.edu>
2023-05-01 21:01:19 -0400

    forgot to add card for new month

--- src/Main/Pages/NewFeatures/newFeatures.js
+++ src/Main/Pages/NewFeatures/newFeatures.js
@@ -7,12 +7,14 @@ import classes from './newFeatures.module.css';
 export default function NewFeatures() {
     return (
         <DefaultPage headerText="Features & Updates Log">
-            <Card headerText="April 2023">
-            <BodyHeader>5/01/2023</BodyHeader>
+            <Card headerText="May 2023">
+                <BodyHeader>5/01/2023</BodyHeader>
                 <div className={classes.updateBody}>
                     Implemented a context for login.  Hiding and showing elements based on login status.  Changing Login menu link to Log Out once user is logged in.
                     Still need to work on database connectivity and api calls, but all in due time.
                 </div>
+            </Card>
+            <Card headerText="April 2023">
                 <BodyHeader>4/24/2023</BodyHeader>
                 <div className={classes.updateBody}>
                     Massive styles overhaul: font spacing, color, sizing.  Created a top navigation bar.  Added all pages belonging to the navigation system.  
