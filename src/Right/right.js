951331ce60a4918eeb5ecb991c02b4e856a42f10
Christopher L Ruckman <cruckman900@cardinal.wju.edu>
2023-05-07 22:21:57 -0400

    added scaling to portrait spinner.  added character spacing to card and portrait

--- src/Right/right.js
+++ src/Right/right.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import classes from './right.module.css';
 import RightContainer from "../UI/RightContainer/RightContainer";
 import Card from "../UI/Card/Card";
 
