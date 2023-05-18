af6fae40ee9445290c7b68a665bd7b5e87615cf2
Christopher L Ruckman <cruckman900@cardinal.wju.edu>
2023-05-18 09:52:22 -0400

    space control editing

--- src/Layout/StatusBar/StatusBar.js
+++ src/Layout/StatusBar/StatusBar.js
@@ -10,11 +10,11 @@ const StatusBar = () => {
         <div className={classes.statusBar}>
             <Status label="Version" value={process.env.REACT_APP_VERSION} />
             <Status label="# Users" value="0" />
-            <Status label="# Online" value="0" />
+            <Status label="# On" value="0" />
             {ctx.isLoggedIn && (
                 <Fragment>
-                    <Status label="# New PMs" value="0" />
-                    <Status label="# New CMs" value="0" />
+                    <Status label="# PMs" value="0" />
+                    <Status label="# CMs" value="0" />
                 </Fragment>
             )}
         </div>
