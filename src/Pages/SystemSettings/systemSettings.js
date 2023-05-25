b5449b5bef9177321c533a6226f3f9b289e2a5fd
Christopher L Ruckman <cruckman900@cardinal.wju.edu>
2023-05-25 14:04:29 -0400

    .....

--- src/Pages/SystemSettings/systemSettings.js
+++ src/Pages/SystemSettings/systemSettings.js
@@ -26,56 +26,52 @@ export default function SystemSettings() {
     return (
         <DefaultPage headerText="System Settings">
             <form onSubmit={onSubmitHandler}>
-                <Row className={classes.row}>
-                    <Col>
-                        <div>
-                            <LeftLabelInput
-                                id="avatarImage"
-                                inputType="file"
-                                labelText="Avatar Image"
-                                inputClassName={classes.inputFileType}
-                                onChange={(event) => imgOnChangeHandler(event)}
-                            />
-                        </div>
-                    </Col>
+                <div className={classes.row}>
+                    <div>
+                        <LeftLabelInput
+                            id="avatarImage"
+                            inputType="file"
+                            labelText="Avatar Image"
+                            inputClassName={classes.inputFileType}
+                            onChange={(event) => imgOnChangeHandler(event)}
+                        />
+                    </div>
                     {imageSrc !== null && (
-                        <Col>
+                        <div>
                             <Label text="Preview" className={classes.Label} />
                             <img src={imageSrc} className={classes.image} />
-                        </Col>
+                        </div>
                     )}
-                </Row>
-                <Row className={classes.row}>
-                    <Col>
+                </div>
+                <div className={classes.row}>
+                    <div>
+                        <LeftLabelInput
+                            id="primaryColor"
+                            inputType="color"
+                            inputClassName={classes.inputColorType}
+                            labelText="Primary Color"
+                            value={primaryColor}
+                            onChange={(event) => setPrimaryColor(event.target.value)}
+                        />
+                    </div>
+                    <div>
+                        <LeftLabelInput
+                            id="secondaryColor"
+                            inputType="color"
+                            inputClassName={classes.inputColorType}
+                            labelText="Secondary Color"
+                            value={secondaryColor}
+                            onChange={(event) => setSecondaryColor(event.target.value)}
+                        />
+                    </div>
+                    <div className={classes.row}>
+                        <Label text="Preview" className={classes.Label} />
                         <div>
-                            <LeftLabelInput
-                                id="primaryColor"
-                                inputType="color"
-                                inputClassName={classes.inputColorType}
-                                labelText="Primary Color"
-                                value={primaryColor}
-                                onChange={(event) => setPrimaryColor(event.target.value)}
-                            />
+                            <div className={classes.span} style={{color: `${primaryColor}`}}>Your Name: </div>
+                            <div className={classes.span} style={{color: `${secondaryColor}`}}>Your Message Here.</div>
                         </div>
-                        <div>
-                            <LeftLabelInput
-                                id="secondaryColor"
-                                inputType="color"
-                                inputClassName={classes.inputColorType}
-                                labelText="Secondary Color"
-                                value={secondaryColor}
-                                onChange={(event) => setSecondaryColor(event.target.value)}
-                            />
-                        </div>
-                        <div className={classes.row}>
-                            <Label text="Preview" className={classes.Label} />
-                            <div>
-                                <div className={classes.span} style={{color: `${primaryColor}`}}>Your Name: </div>
-                                <div className={classes.span} style={{color: `${secondaryColor}`}}>Your Message Here.</div>
-                            </div>
-                        </div>
-                    </Col>
-                </Row>
+                    </div>
+                </div>
 
                 <BodyHeader>&nbsp;</BodyHeader>
                 <div className={classes.formRow}>
