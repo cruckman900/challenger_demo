82cb5eefff8080323cc73bcf218939c85c682229
Christopher L Ruckman <cruckman900@cardinal.wju.edu>
2023-05-25 22:40:46 -0400

    made it so that some status bar items are visible on mobile. adjusted modal

--- src/Layout/StatusBar/Status.js
+++ src/Layout/StatusBar/Status.js
@@ -1,8 +1,9 @@
 import classes from './Status.module.css';
 
 const Status = (props) => {
+    console.log(props.className);
     return (
-        <div className={classes.statusContainer}>
+        <div className={`${classes.statusContainer} ${props.className}`}>
             <div className={classes.statusLabel}>{props.label}</div>
             <div className={classes.status}>{props.value}</div>
         </div>
--- src/Layout/StatusBar/StatusBar.js
+++ src/Layout/StatusBar/StatusBar.js
@@ -4,14 +4,16 @@ import Status from './Status';
 import classes from './StatusBar.module.css';
 
 const StatusBar = () => {
-    const ctx = useContext(AuthContext);
+    const authCtx = useContext(AuthContext);
 
     return (
         <div className={classes.statusBar}>
-            <Status label="Version" value={process.env.REACT_APP_VERSION} />
-            <Status label="# Users" value="0" />
-            <Status label="# On" value="0" />
-            {ctx.isLoggedIn && (
+            <Fragment>
+                <Status className={classes.statusHideIfTiny} label="Version" value={process.env.REACT_APP_VERSION} />
+                <Status className={classes.statusHideIfTiny} label="# Users" value="0" />
+                <Status className={classes.statusHideIfTiny} label="# On" value="0" />
+            </Fragment>
+            {authCtx.isLoggedIn && (
                 <Fragment>
                     <Status label="# PMs" value="0" />
                     <Status label="# CMs" value="0" />
