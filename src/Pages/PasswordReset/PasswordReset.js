8471f48eb083df7ea753dcd9ecd6558c6a103fde
Christopher L Ruckman <cruckman900@cardinal.wju.edu>
2023-05-28 02:01:43 -0400

    passwordreset page stub

---
+++ src/Pages/PasswordReset/PasswordReset.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import DefaultPage from '../../UI/DefaultPage/DefaultPage';
+
+const PasswordReset = () => {
+    return (
+        <DefaultPage headerText="Password Reset">
+            
+        </DefaultPage>
+    )
+}
