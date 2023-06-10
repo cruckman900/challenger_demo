480f3fd698007cd622db241367cffdba13d00d21
Christopher L Ruckman <cruckman900@cardinal.wju.edu>
2023-06-10 11:31:38 -0400

    cleaned Main for eslint

--- src/Layout/Main/Main.js
+++ src/Layout/Main/Main.js
@@ -1,19 +1,19 @@
-import React, { Component } from "react";
-import { Routes, Route } from 'react-router-dom';
-import Home from '../../Pages/Home/home';
-import Login from '../../Pages/Login/login';
-import Donations from '../../Pages/Donations/donations';
-import Suggestions from '../../Pages/Suggestions/suggestions';
-import UserSettings from '../../Pages/UserSettings/UserSettings';
-import FriendSettings from '../../Pages/FriendSettings/friendSettings';
-import GroupSettings from '../../Pages/GroupSettings/groupSettings';
-import CommunitySettings from '../../Pages/CommunitySettings/communitySettings';
-import NewFeatures from '../../Pages/NewFeatures/newFeatures';
-import SystemSettings from '../../Pages/SystemSettings/systemSettings';
-import classes from './main.module.css';
+import React, { Component } from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from '../../Pages/Home/home'
+import Login from '../../Pages/Login/login'
+import Donations from '../../Pages/Donations/donations'
+import Suggestions from '../../Pages/Suggestions/suggestions'
+import UserSettings from '../../Pages/UserSettings/UserSettings'
+import FriendSettings from '../../Pages/FriendSettings/friendSettings'
+import GroupSettings from '../../Pages/GroupSettings/groupSettings'
+import CommunitySettings from '../../Pages/CommunitySettings/communitySettings'
+import NewFeatures from '../../Pages/NewFeatures/newFeatures'
+import SystemSettings from '../../Pages/SystemSettings/systemSettings'
+import classes from './main.module.css'
 
 class Main extends Component {
-    render() {
+    render () {
         return (
             <div className={`${classes.main} ${classes.scrollable} ${classes.noScrollbars}`}>
                 <Routes>
@@ -29,8 +29,8 @@ class Main extends Component {
                     <Route path="/systemSettings" element={ <SystemSettings /> } />
                 </Routes>
             </div>
-        );
+        )
     }
 }
 
-export default Main;
+export default Main
