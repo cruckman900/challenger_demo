a30cdab4ae22a5fd07bb74386a1583022eb7c155
Christopher L Ruckman <cruckman900@cardinal.wju.edu>
2023-06-10 10:43:45 -0400

    cleaned suggestions for eslint

--- src/Pages/Suggestions/suggestions.js
+++ src/Pages/Suggestions/suggestions.js
@@ -1,15 +1,16 @@
-import React, { Fragment } from "react";
-import DefaultPage from "../../UI/DefaultPage/DefaultPage";
+/* eslint-disable react/no-unescaped-entities */
+import React, { Fragment } from 'react'
+import DefaultPage from '../../UI/DefaultPage/DefaultPage'
 
-export default function Suggestions() {
+export default function Suggestions () {
     return (
         <Fragment>
             <DefaultPage headerText="Suggestions">
                 <div>
-                    If you have an idea of what you'd like to see happen on this site, drop a suggestion, or vote on 
+                    If you have an idea of what you'd like to see happen on this site, drop a suggestion, or vote on
                     one that has already been posted.  I'll try to work on them in order of the number of votes.
                 </div>
             </DefaultPage>
         </Fragment>
-    );
+    )
 }
