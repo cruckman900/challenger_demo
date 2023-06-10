ce9d7ee980c75bf633428b8cfd0a733d09c9d5ae
Christopher L Ruckman <cruckman900@cardinal.wju.edu>
2023-06-10 11:32:26 -0400

    cleaned Left for eslint

--- src/Layout/Left/left.js
+++ src/Layout/Left/left.js
@@ -1,16 +1,16 @@
-import React, { useContext } from "react";
-import AuthContext from "../../store/auth-context";
-import { Navbar, Nav } from "react-bootstrap";
-import { Link, useLocation } from 'react-router-dom';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { 
+import React, { useContext } from 'react'
+import AuthContext from '../../store/auth-context'
+import { Navbar, Nav } from 'react-bootstrap'
+import { Link, useLocation } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
     faHouse, faUserGear, faUsersGear, faPeopleGroup, faPeopleRoof, faNewspaper, faGear
-} from '@fortawesome/free-solid-svg-icons';
-import classes from './left.module.css';
+} from '@fortawesome/free-solid-svg-icons'
+import classes from './left.module.css'
 
 const Left = () => {
-    const ctx = useContext(AuthContext);
-    const location = useLocation();
+    const ctx = useContext(AuthContext)
+    const location = useLocation()
 
     const myIcons = {
         house: faHouse,
@@ -19,42 +19,42 @@ const Left = () => {
         usersGear: faUsersGear,
         peopleGroup: faPeopleGroup,
         peopleRoof: faPeopleRoof,
-        gear: faGear,
+        gear: faGear
     }
-    
+
     return (
         <React.Fragment>
             <Navbar key="left" className={`${classes.left} ${classes.scrollable} ${classes.noScrollbars}`}>
                 <Nav defaultActiveKey="#" activeKey={location.pathname}>
-                    <Nav.Link as={Link} to={"/"} eventKey="/" href="#" alt="Home">
-                        <p className={classes.menuButtonBackground}><FontAwesomeIcon className={classes.menuButton} icon={myIcons['house']} /></p>
+                    <Nav.Link as={Link} to={'/'} eventKey="/" href="#" alt="Home">
+                        <p className={classes.menuButtonBackground}><FontAwesomeIcon className={classes.menuButton} icon={myIcons.house} /></p>
                     </Nav.Link>
-                    <Nav.Link as={Link} to={"/userSettings"} eventKey="/userSettings" href="#userSettings">
-                        <p className={classes.menuButtonBackground}><FontAwesomeIcon className={classes.menuButton} icon={myIcons['userGear']} /></p>
+                    <Nav.Link as={Link} to={'/userSettings'} eventKey="/userSettings" href="#userSettings">
+                        <p className={classes.menuButtonBackground}><FontAwesomeIcon className={classes.menuButton} icon={myIcons.userGear} /></p>
                     </Nav.Link>
-                    {ctx.isLoggedIn && 
+                    {ctx.isLoggedIn &&
                         <>
-                            <Nav.Link as={Link} to={"/friendSettings"} eventKey="/friendSettings" href="#friendSettings">
-                                <p className={classes.menuButtonBackground}><FontAwesomeIcon className={classes.menuButton} icon={myIcons['usersGear']} /></p>
+                            <Nav.Link as={Link} to={'/friendSettings'} eventKey="/friendSettings" href="#friendSettings">
+                                <p className={classes.menuButtonBackground}><FontAwesomeIcon className={classes.menuButton} icon={myIcons.usersGear} /></p>
                             </Nav.Link>
-                            <Nav.Link as={Link} to={"/groupSettings"} eventKey="/groupSettings" href="#groupSettings">
-                                <p className={classes.menuButtonBackground}><FontAwesomeIcon className={classes.menuButton} icon={myIcons['peopleGroup']} /></p>
+                            <Nav.Link as={Link} to={'/groupSettings'} eventKey="/groupSettings" href="#groupSettings">
+                                <p className={classes.menuButtonBackground}><FontAwesomeIcon className={classes.menuButton} icon={myIcons.peopleGroup} /></p>
                             </Nav.Link>
-                            <Nav.Link as={Link} to={"/communitySettings"} eventKey="/communitySettings" href="#communitySettings">
-                                <p className={classes.menuButtonBackground}><FontAwesomeIcon className={classes.menuButton} icon={myIcons['peopleRoof']} /></p>
+                            <Nav.Link as={Link} to={'/communitySettings'} eventKey="/communitySettings" href="#communitySettings">
+                                <p className={classes.menuButtonBackground}><FontAwesomeIcon className={classes.menuButton} icon={myIcons.peopleRoof} /></p>
                             </Nav.Link>
-                            <Nav.Link as={Link} to={"/systemSettings"} eventKey="/systemSettings" href="#systemSettings">
-                                <p className={classes.menuButtonBackground}><FontAwesomeIcon className={classes.menuButton} icon={myIcons['gear']} /></p>
+                            <Nav.Link as={Link} to={'/systemSettings'} eventKey="/systemSettings" href="#systemSettings">
+                                <p className={classes.menuButtonBackground}><FontAwesomeIcon className={classes.menuButton} icon={myIcons.gear} /></p>
                             </Nav.Link>
-                        </>                    
+                        </>
                     }
-                    <Nav.Link as={Link} to={"/newFeatures"} eventKey="/newFeatures" href="#newFeatures">
-                        <p className={classes.menuButtonBackground}><FontAwesomeIcon className={classes.menuButton} icon={myIcons['newspaper']} /></p>
+                    <Nav.Link as={Link} to={'/newFeatures'} eventKey="/newFeatures" href="#newFeatures">
+                        <p className={classes.menuButtonBackground}><FontAwesomeIcon className={classes.menuButton} icon={myIcons.newspaper} /></p>
                     </Nav.Link>
                 </Nav>
             </Navbar>
         </React.Fragment>
-    );
+    )
 }
 
-export default Left;
+export default Left
