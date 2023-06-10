d1c2e9a5957be693264801e49585f5913373379b
Christopher L Ruckman <cruckman900@cardinal.wju.edu>
2023-06-10 04:03:20 -0400

    cleaned index for eslint

--- src/index.js
+++ src/index.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
-import { AuthContextProvider } from './store/auth-context';
-import { NavContextProvider } from './store/nav-context';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import App from './App'
+import reportWebVitals from './reportWebVitals'
+import { BrowserRouter } from 'react-router-dom'
+import { AuthContextProvider } from './store/auth-context'
+import { NavContextProvider } from './store/nav-context'
 
-console.log(`${process.env.REACT_APP_NAME} ${process.env.REACT_APP_VERSION}`);
+console.log(`${process.env.REACT_APP_NAME} ${process.env.REACT_APP_VERSION}`)
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <BrowserRouter>
-    <AuthContextProvider>
-      <NavContextProvider>
-        <App />
-      </NavContextProvider>
-    </AuthContextProvider>
-  </BrowserRouter>
-);
+    <BrowserRouter>
+        <AuthContextProvider>
+            <NavContextProvider>
+                <App />
+            </NavContextProvider>
+        </AuthContextProvider>
+    </BrowserRouter>
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
