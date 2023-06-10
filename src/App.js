b1453a0818f4e02f0c925d8e852b5240a95ed45e
Christopher L Ruckman <cruckman900@cardinal.wju.edu>
2023-06-10 11:42:42 -0400

    cleaned App for eslint

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
-import React, { Component } from 'react';
-import { Row, Col } from 'react-bootstrap';
-import Top from './Layout/Top/top';
-import Left from './Layout/Left/left';
-import Main from './Layout/Main/Main';
-import Right from './Layout/Right/right';
-import StatusBar from './Layout/StatusBar/StatusBar';
-import Footer from './Layout/Footer/footer';
-import classes from './App.module.css';
+import React, { Component } from 'react'
+import { Row, Col } from 'react-bootstrap'
+import Top from './Layout/Top/top'
+import Left from './Layout/Left/left'
+import Main from './Layout/Main/Main'
+import Right from './Layout/Right/right'
+import StatusBar from './Layout/StatusBar/StatusBar'
+import Footer from './Layout/Footer/footer'
+import classes from './App.module.css'
 
 class App extends Component {
-  render() {
-    return (
-      <div className={classes.App}>
-        <Row><Col><Top /></Col></Row>
-        <Row className={classes.horizRowContainer}>
-          <Col><Left /></Col>
-          <Col><Main /></Col>
-          <Col className={classes.floatingHolder}><Right /></Col>
-        </Row>
-        <Row className={classes.status}><Col><StatusBar /></Col></Row>
-        <Row><Col><Footer /></Col></Row>
-      </div>
-    );
-  }
+    render () {
+        return (
+            <div className={classes.App}>
+                <Row><Col><Top /></Col></Row>
+                <Row className={classes.horizRowContainer}>
+                    <Col><Left /></Col>
+                    <Col><Main /></Col>
+                    <Col className={classes.floatingHolder}><Right /></Col>
+                </Row>
+                <Row className={classes.status}><Col><StatusBar /></Col></Row>
+                <Row><Col><Footer /></Col></Row>
+            </div>
+        )
+    }
 }
 
-export default App;
+export default App
