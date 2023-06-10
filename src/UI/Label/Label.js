9792ab025665bad84daf3ca2ad2d544ddc259a81
Christopher L Ruckman <cruckman900@cardinal.wju.edu>
2023-06-10 10:11:44 -0400

    cleaned Label for eslint

--- src/UI/Label/Label.js
+++ src/UI/Label/Label.js
@@ -1,7 +1,9 @@
-import classes from './Label.module.css';
+/* eslint-disable react/prop-types */
+import React from 'react'
+import classes from './Label.module.css'
 
-export default function Label(props) {
+export default function Label (props) {
     return (
         <label className={`${classes.label} ${props.className}`} htmlFor={props.htmlFor}>{props.text}</label>
-    );
+    )
 }
