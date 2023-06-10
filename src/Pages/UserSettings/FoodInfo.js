e96f9b6a6cb11f01ff609889cfd4b8c1bee14733
Christopher L Ruckman <cruckman900@cardinal.wju.edu>
2023-06-10 10:38:29 -0400

    cleaned FoodInfo for eslint

--- src/Pages/UserSettings/FoodInfo.js
+++ src/Pages/UserSettings/FoodInfo.js
@@ -1,31 +1,32 @@
-import React from "react";
-import BodyHeader from "../../UI/BodyHeader/BodyHeader";
-import Button from "../../UI/Button/Button";
-import labeledInputs from '../../builders/LabeledInputs/labeledInputs';
-import classes from './UserSettings.module.css';
+/* eslint-disable react/prop-types */
+import React from 'react'
+import BodyHeader from '../../UI/BodyHeader/BodyHeader'
+import Button from '../../UI/Button/Button'
+import labeledInputs from '../../builders/LabeledInputs/labeledInputs'
+import classes from './UserSettings.module.css'
 
 const FoodInfo = (props) => {
     const onSubmitHandler = (event) => {
-        event.preventDefault();
-    };
+        event.preventDefault()
+    }
 
     const inputs = [
-        {id: "chkAmerican", name: "cuisinetypes", inputType: "checkbox", required: false, labelText: "American?", value: props.chkAmerican},
-        {id: "chkAsian", name: "cuisinetypes", inputType: "checkbox", required: false, labelText: "Asian / Indian", value: props.chkAsian},
-        {id: "chkCajun", name: "cuisinetypes", inputType: "checkbox", required: false, labelText: "Cajun", value: props.chkCajun},
-        {id: "chkFrench", name: "cuisinetypes", inputType: "checkbox", required: false, labelText: "French", value: props.chkFrench},
-        {id: "chkHungarian", name: "cuisinetypes", inputType: "checkbox", required: false, labelText: "Hungarian", value: props.chkHungarian},
-        {id: "chkItalian", name: "cuisinetypes", inputType: "checkbox", required: false, labelText: "Italian", value: props.chkItalian},
-        {id: "chkMediterranean", name: "cuisinetypes", inputType: "checkbox", required: false, labelText: "Mediterranean", value: props.chkMediterranian},
-        {id: "chkMexican", name: "cuisinetypes", inputType: "checkbox", required: false, labelText: "Latin / Mexican", value: props.chkMexican},
-        {id: "chkRomanian", name: "cuisinetypes", inputType: "checkbox", required: false, labelText: "Romanian", value: props.chkRomanian},
-        {id: "chkRussian", name: "cuisinetypes", inputType: "checkbox", required: false, labelText: "Russian", value: props.chkRussian},
-        {id: "chkSlavic", name: "cuisinetypes", inputType: "checkbox", required: false, labelText: "Slavic", value: props.chkSlavic},
-        {id: "chkCookies", name: "cuisinetypes", inputType: "checkbox", required: false, labelText: "I only like cookies", value: props.chkCookies},
-        {id: "chkFoodOther", name: "cuisinetypes", inputType: "checkbox", required: false, labelText: "Other", value: props.chkFoodOther},
-    ];
+        { id: 'chkAmerican', name: 'cuisinetypes', inputType: 'checkbox', required: false, labelText: 'American?', value: props.chkAmerican },
+        { id: 'chkAsian', name: 'cuisinetypes', inputType: 'checkbox', required: false, labelText: 'Asian / Indian', value: props.chkAsian },
+        { id: 'chkCajun', name: 'cuisinetypes', inputType: 'checkbox', required: false, labelText: 'Cajun', value: props.chkCajun },
+        { id: 'chkFrench', name: 'cuisinetypes', inputType: 'checkbox', required: false, labelText: 'French', value: props.chkFrench },
+        { id: 'chkHungarian', name: 'cuisinetypes', inputType: 'checkbox', required: false, labelText: 'Hungarian', value: props.chkHungarian },
+        { id: 'chkItalian', name: 'cuisinetypes', inputType: 'checkbox', required: false, labelText: 'Italian', value: props.chkItalian },
+        { id: 'chkMediterranean', name: 'cuisinetypes', inputType: 'checkbox', required: false, labelText: 'Mediterranean', value: props.chkMediterranian },
+        { id: 'chkMexican', name: 'cuisinetypes', inputType: 'checkbox', required: false, labelText: 'Latin / Mexican', value: props.chkMexican },
+        { id: 'chkRomanian', name: 'cuisinetypes', inputType: 'checkbox', required: false, labelText: 'Romanian', value: props.chkRomanian },
+        { id: 'chkRussian', name: 'cuisinetypes', inputType: 'checkbox', required: false, labelText: 'Russian', value: props.chkRussian },
+        { id: 'chkSlavic', name: 'cuisinetypes', inputType: 'checkbox', required: false, labelText: 'Slavic', value: props.chkSlavic },
+        { id: 'chkCookies', name: 'cuisinetypes', inputType: 'checkbox', required: false, labelText: 'I only like cookies', value: props.chkCookies },
+        { id: 'chkFoodOther', name: 'cuisinetypes', inputType: 'checkbox', required: false, labelText: 'Other', value: props.chkFoodOther }
+    ]
 
-    const formInputs = labeledInputs(inputs);
+    const formInputs = labeledInputs(inputs)
 
     return (
         <form onSubmit={onSubmitHandler}>
@@ -36,7 +37,7 @@ const FoodInfo = (props) => {
                 <Button className={classes.primaryBtn} type="submit" name="btnSubmit" value="Submit" />
             </div>
         </form>
-    );
-};
+    )
+}
 
-export default FoodInfo;
+export default FoodInfo
