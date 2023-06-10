e6d27cde8c528814c3b4225b65641a1e229a7bcc
Christopher L Ruckman <cruckman900@cardinal.wju.edu>
2023-06-10 10:21:04 -0400

    cleaned MovieInfo for eslint

--- src/Pages/UserSettings/MovieInfo.js
+++ src/Pages/UserSettings/MovieInfo.js
@@ -1,33 +1,34 @@
-import React from "react";
-import BodyHeader from "../../UI/BodyHeader/BodyHeader";
-import Button from "../../UI/Button/Button";
-import labeledInputs from '../../builders/LabeledInputs/labeledInputs';
-import classes from './UserSettings.module.css';
+/* eslint-disable react/prop-types */
+import React from 'react'
+import BodyHeader from '../../UI/BodyHeader/BodyHeader'
+import Button from '../../UI/Button/Button'
+import labeledInputs from '../../builders/LabeledInputs/labeledInputs'
+import classes from './UserSettings.module.css'
 
 const MovieInfo = (props) => {
     const onSubmitHandler = (event) => {
-        event.preventDefault();
-    };
+        event.preventDefault()
+    }
 
     const inputs = [
-        {id: "chkAction", name: "literaturetypes", className: classes.smallInputStyle, inputType: "checkbox", required: false, labelText: "Action", value: props.chkAction},
-        {id: "chkComedy", name: "literaturetypes", className: classes.smallInputStyle, inputType: "checkbox", required: false, labelText: "Comedy", value: props.chkComedy},
-        {id: "chkAnimation", name: "literaturetypes", className: classes.smallInputStyle, inputType: "checkbox", required: false, labelText: "Comics / Animation", value: props.chkAnimation},
-        {id: "chkDocumentary", name: "literaturetypes", className: classes.smallInputStyle, inputType: "checkbox", required: false, labelText: "Documentary", value: props.chkDocumentary},
-        {id: "chkDrama", name: "literaturetypes", className: classes.smallInputStyle, inputType: "checkbox", required: false, labelText: "Drama", value: props.chkDrama},
-        {id: "chkHistory", name: "literaturetypes", className: classes.smallInputStyle, inputType: "checkbox", required: false, labelText: "History", value: props.chkHistory},
-        {id: "chkMystery", name: "literaturetypes", className: classes.smallInputStyle, inputType: "checkbox", required: false, labelText: "Mystery", value: props.chkMystery},
-        {id: "chkNature", name: "literaturetypes", className: classes.smallInputStyle, inputType: "checkbox", required: false, labelText: "Nature", value: props.chkNature},
-        {id: "chkNews", name: "literaturetypes", className: classes.smallInputStyle, inputType: "checkbox", required: false, labelText: "News / World Affairs", value: props.chkNews},
-        {id: "chkReligion", name: "literaturetypes", className: classes.smallInputStyle, inputType: "checkbox", required: false, labelText: "Religion", value: props.chkReligion},
-        {id: "chkRomance", name: "literaturetypes", className: classes.smallInputStyle, inputType: "checkbox", required: false, labelText: "Romance", value: props.chkRomance},
-        {id: "chkSciFi", name: "literaturetypes", className: classes.smallInputStyle, inputType: "checkbox", required: false, labelText: "Sci-Fi", value: props.chkSciFi},
-        {id: "chkSports", name: "literaturetypes", className: classes.smallInputStyle, inputType: "checkbox", required: false, labelText: "Sports", value: props.chkSports},
-        {id: "chkThriller", name: "literaturetypes", className: classes.smallInputStyle, inputType: "checkbox", required: false, labelText: "Suspense / Thriller", value: props.chkThriller},
-        {id: "chkOther", name: "literaturetypes", className: classes.smallInputStyle, inputType: "checkbox", required: false, labelText: "Other", value: props.chkLitOther},
-    ];
+        { id: 'chkAction', name: 'literaturetypes', className: classes.smallInputStyle, inputType: 'checkbox', required: false, labelText: 'Action', value: props.chkAction },
+        { id: 'chkComedy', name: 'literaturetypes', className: classes.smallInputStyle, inputType: 'checkbox', required: false, labelText: 'Comedy', value: props.chkComedy },
+        { id: 'chkAnimation', name: 'literaturetypes', className: classes.smallInputStyle, inputType: 'checkbox', required: false, labelText: 'Comics / Animation', value: props.chkAnimation },
+        { id: 'chkDocumentary', name: 'literaturetypes', className: classes.smallInputStyle, inputType: 'checkbox', required: false, labelText: 'Documentary', value: props.chkDocumentary },
+        { id: 'chkDrama', name: 'literaturetypes', className: classes.smallInputStyle, inputType: 'checkbox', required: false, labelText: 'Drama', value: props.chkDrama },
+        { id: 'chkHistory', name: 'literaturetypes', className: classes.smallInputStyle, inputType: 'checkbox', required: false, labelText: 'History', value: props.chkHistory },
+        { id: 'chkMystery', name: 'literaturetypes', className: classes.smallInputStyle, inputType: 'checkbox', required: false, labelText: 'Mystery', value: props.chkMystery },
+        { id: 'chkNature', name: 'literaturetypes', className: classes.smallInputStyle, inputType: 'checkbox', required: false, labelText: 'Nature', value: props.chkNature },
+        { id: 'chkNews', name: 'literaturetypes', className: classes.smallInputStyle, inputType: 'checkbox', required: false, labelText: 'News / World Affairs', value: props.chkNews },
+        { id: 'chkReligion', name: 'literaturetypes', className: classes.smallInputStyle, inputType: 'checkbox', required: false, labelText: 'Religion', value: props.chkReligion },
+        { id: 'chkRomance', name: 'literaturetypes', className: classes.smallInputStyle, inputType: 'checkbox', required: false, labelText: 'Romance', value: props.chkRomance },
+        { id: 'chkSciFi', name: 'literaturetypes', className: classes.smallInputStyle, inputType: 'checkbox', required: false, labelText: 'Sci-Fi', value: props.chkSciFi },
+        { id: 'chkSports', name: 'literaturetypes', className: classes.smallInputStyle, inputType: 'checkbox', required: false, labelText: 'Sports', value: props.chkSports },
+        { id: 'chkThriller', name: 'literaturetypes', className: classes.smallInputStyle, inputType: 'checkbox', required: false, labelText: 'Suspense / Thriller', value: props.chkThriller },
+        { id: 'chkOther', name: 'literaturetypes', className: classes.smallInputStyle, inputType: 'checkbox', required: false, labelText: 'Other', value: props.chkLitOther }
+    ]
 
-    const formInputs = labeledInputs(inputs);
+    const formInputs = labeledInputs(inputs)
 
     return (
         <form onSubmit={onSubmitHandler}>
@@ -38,7 +39,7 @@ const MovieInfo = (props) => {
                 <Button className={classes.primaryBtn} type="submit" name="btnSubmit" value="Submit" />
             </div>
         </form>
-    );
-};
+    )
+}
 
-export default MovieInfo;
+export default MovieInfo
