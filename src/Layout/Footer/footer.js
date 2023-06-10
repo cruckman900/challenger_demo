277a6a6cfe1843845cf21e20d29f9937ad648194
Christopher L Ruckman <cruckman900@cardinal.wju.edu>
2023-06-10 11:33:57 -0400

    cleaned footer for eslint

--- src/Layout/Footer/footer.js
+++ src/Layout/Footer/footer.js
@@ -1,10 +1,10 @@
-import React from "react";
-import classes from './footer.module.css';
+import React from 'react'
+import classes from './footer.module.css'
 
-export default function Footer() {
+export default function Footer () {
     return (
         <footer className={classes.footer}>
             &copy;{new Date().getFullYear()} CHATTERBOXSM &mdash; ALL RIGHTS RESERVED &nbsp;
         </footer>
-    );
+    )
 }
