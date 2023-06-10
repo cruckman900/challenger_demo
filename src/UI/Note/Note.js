60651c4b3183335acf254148a592e99483f520d4
Christopher L Ruckman <cruckman900@cardinal.wju.edu>
2023-06-10 09:53:51 -0400

    cleaned Note for eslint

--- src/UI/Note/Note.js
+++ src/UI/Note/Note.js
@@ -1,19 +1,20 @@
-import React, { useState, useEffect } from "react";
-import classes from './Note.module.css';
+/* eslint-disable react/prop-types */
+import React, { useState, useEffect } from 'react'
+import classes from './Note.module.css'
 
-export default function Note(props) {
-    const [headerStyle, setHeaderStyle] = useState('note');
-    const [bodyStyle, setBodyStyle] = useState('note');
+export default function Note (props) {
+    const [headerStyle, setHeaderStyle] = useState('note')
+    const [bodyStyle, setBodyStyle] = useState('note')
 
     useEffect(() => {
-        setHeaderStyle(props.noteType + "type-header");
-        setBodyStyle(props.noteType + "type-body");
-    }, [props.noteType]);
+        setHeaderStyle(props.noteType + 'type-header')
+        setBodyStyle(props.noteType + 'type-body')
+    }, [props.noteType])
 
     return (
         <div className={classes.note}>
             <div className={`${classes.noteHeader} ${classes[headerStyle]}`}>{props.headerText}</div>
-            <div className={`${classes.noteBody} ${classes[bodyStyle]}`} style={{bodyStyle}}>{props.children}</div>
+            <div className={`${classes.noteBody} ${classes[bodyStyle]}`} style={{ bodyStyle }}>{props.children}</div>
         </div>
-    );
+    )
 }
