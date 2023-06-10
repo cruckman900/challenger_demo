a8f849fab0b533d264938adada2974c8815f3fe4
Christopher L Ruckman <cruckman900@cardinal.wju.edu>
2023-06-10 03:59:32 -0400

    cleaned auth-context for eslint

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -1,4 +1,6 @@
-import React, { useState, useEffect } from 'react';
+/* eslint-disable react/prop-types */
+/* eslint-disable object-shorthand */
+import React, { useState, useEffect } from 'react'
 
 const AuthContext = React.createContext({
     isLoggedIn: false,
@@ -6,40 +8,40 @@ const AuthContext = React.createContext({
     user: {},
     onLogout: () => {},
     onLogin: () => {}
-});
+})
 
 export const AuthContextProvider = (props) => {
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
-    const [userID, setUserID] = useState(null);
-    const [user, setUser] = useState({});
+    const [isLoggedIn, setIsLoggedIn] = useState(false)
+    const [userID, setUserID] = useState(null)
+    const [user, setUser] = useState({})
 
     useEffect(() => {
-        const storedUserLoggedInInformation = localStorage.getItem('isLoggedIn');
-        const storedUserID = localStorage.getItem('userID');
-        const storedUser = localStorage.getItem('storedUser');
+        const storedUserLoggedInInformation = localStorage.getItem('isLoggedIn')
+        const storedUserID = localStorage.getItem('userID')
+        const storedUser = localStorage.getItem('storedUser')
 
-        if(storedUserLoggedInInformation === '1') setIsLoggedIn(true);
-        if(storedUserID !== null) setUserID(storedUserID);
-        if (storedUserLoggedInInformation !== null) setUser(storedUser);
-    }, []);
+        if (storedUserLoggedInInformation === '1') setIsLoggedIn(true)
+        if (storedUserID !== null) setUserID(storedUserID)
+        if (storedUserLoggedInInformation !== null) setUser(storedUser)
+    }, [])
 
     const logoutHandler = () => {
-        localStorage.removeItem('isLoggedIn');
-        setIsLoggedIn(false);
-        localStorage.removeItem('userID');
-        setUserID(null);
-        localStorage.removeItem('storedUser');
-        setUser({});
-    };
+        localStorage.removeItem('isLoggedIn')
+        setIsLoggedIn(false)
+        localStorage.removeItem('userID')
+        setUserID(null)
+        localStorage.removeItem('storedUser')
+        setUser({})
+    }
 
     const loginHandler = (userID, user) => {
-        localStorage.setItem('isLoggedIn', '1');
-        setIsLoggedIn(true);
-        localStorage.setItem('userID', userID);
-        setUserID(userID);
-        localStorage.setItem('storedUser', user);
-        setUser(user);
-    };
+        localStorage.setItem('isLoggedIn', '1')
+        setIsLoggedIn(true)
+        localStorage.setItem('userID', userID)
+        setUserID(userID)
+        localStorage.setItem('storedUser', user)
+        setUser(user)
+    }
 
     return <AuthContext.Provider
         value={{
@@ -54,4 +56,4 @@ export const AuthContextProvider = (props) => {
     </AuthContext.Provider>
 }
 
-export default AuthContext;
+export default AuthContext
