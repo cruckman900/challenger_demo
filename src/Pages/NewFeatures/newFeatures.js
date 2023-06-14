5665965da70e0104a7e8ea36bc9224b98376962e
Christopher L Ruckman <cruckman900@cardinal.wju.edu>
2023-06-14 09:47:39 -0400

    updated newFeatures

--- src/Pages/NewFeatures/newFeatures.js
+++ src/Pages/NewFeatures/newFeatures.js
@@ -10,6 +10,10 @@ export default function NewFeatures () {
     return (
         <DefaultPage headerText="Features & Updates Log">
             <Card headerText="June 2023">
+                <BodyHeader className={classes.header}>6/13/2023</BodyHeader>
+                <div className={classes.updateBody}>
+                    Worked out a lot of the api files.  Started working on the api call functions inside the main app.
+                </div>
                 <BodyHeader className={classes.header}>6/10/2023</BodyHeader>
                 <div className={classes.updateBody}>
                     Added a few extensions to VS Code, including ESLint.  I linted my project and revised all files so that they now pass the
