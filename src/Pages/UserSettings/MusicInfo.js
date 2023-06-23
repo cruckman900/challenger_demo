260fb8047587da6b7cb8d7ea4b295b7e4533ffa2
Christopher L Ruckman <cruckman900@cardinal.wju.edu>
2023-06-23 07:14:04 -0400

    removed console.logs

--- src/Pages/UserSettings/MusicInfo.js
+++ src/Pages/UserSettings/MusicInfo.js
@@ -35,6 +35,7 @@ const MusicInfo = (props) => {
         getMusicByUserID(authCtx.userID)
             .then((user) => {
                 const thisUserMusic = user.data.length > 0 ? user.data[0] : null
+                console.log('MusicInfo.js setUpdateState thisUserMusic', thisUserMusic)
                 setUserMusic(thisUserMusic)
 
                 if (thisUserMusic === null) {
